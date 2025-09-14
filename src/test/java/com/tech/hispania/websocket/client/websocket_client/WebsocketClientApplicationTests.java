0c3b26f98a65dfee685417b0ae4a4c7eef119909
Enrique Sanchez <esanchez.devel@gmail.com>
2025-09-14 11:32:53 +0200

    feat: project creation

---
+++ src/test/java/com/tech/hispania/websocket/client/websocket_client/WebsocketClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.hispania.websocket.client.websocket_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
